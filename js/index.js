0322f0470b73edb8d6ed9b387d0f7e28d514b44b
Pilli3800 <l.alvarezaguilar448@gmail.com>
2022-05-21 11:51:38 -0500

    Animacion

--- js/index.js
+++ js/index.js
@@ -1,12 +1,51 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
+const navLink1 = document.querySelector(".nav-menu-item-nosotros");
+const navLink2 = document.querySelector(".nav-menu-item-contacto");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_visible");
-
   if (navMenu.classList.contains("nav-menu_visible")) {
     navToggle.setAttribute("aria-label", "Cerrar menú");
   } else {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
+});
+
+navLink1.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+  console.log("Hola Mundo");
+});
+
+navLink2.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+  console.log("Hola Mundo");
+});
+
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("nav-menu-link").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        100,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
 });
