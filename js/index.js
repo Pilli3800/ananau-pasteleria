85d16d04cc2aab167abfcfc653732b54250a59cd
Pilli3800 <l.alvarezaguilar448@gmail.com>
2022-05-23 15:07:54 -0500

    Ac

--- js/index.js
+++ js/index.js
@@ -48,3 +48,21 @@ $(document).ready(function () {
     } // End if
   });
 });
+
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 200);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
